970bd7f2eec600084c1dfb289aa3a646f8419669
shashidar mandalozu <sasitech18@gmail.com>
2020-06-28 12:56:35 +0530

    React Project-Ravenous:Part2->Passing Information Between Components

--- 46.ravenous/src/components/App/App.js
+++ 46.ravenous/src/components/App/App.js
@@ -3,13 +3,34 @@ import "./App.css";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 
+const business = {
+  imageSrc: "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Bordertown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>Ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
--- 46.ravenous/src/components/BusinessList/BusinessList.js
+++ 46.ravenous/src/components/BusinessList/BusinessList.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import './BusinessList.css';
-import Business from '../Business/Business';
+import React from "react";
+import "./BusinessList.css";
+import Business from "../Business/Business";
 
 class BusinessList extends React.Component {
-    render() {
-        return (
-          <div className="BusinessList">
+  render() {
+    return (
+      <div className="BusinessList">
+        {/* <Business />
             <Business />
             <Business />
             <Business />
             <Business />
-            <Business />
-            <Business />
-          </div>
-        );
+            <Business /> */}
+        {this.props.businesses.map((business) => {
+          return <Business business={business} />
+        })}
+      </div>
+    );
   }
 }
 
