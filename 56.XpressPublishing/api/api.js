72a65a51bf7c2210338f1dab0522c237fe9d66ae
shashidar mandalozu <sasitech18@gmail.com>
2020-07-23 11:08:26 +0530

    Building API Using Express.js & SQLite:Created API Router

---
+++ 56.XpressPublishing/api/api.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const apiRouter = express.Router();
+
+module.exports = apiRouter;
--- 56.XpressPublishing/server.js
+++ 56.XpressPublishing/server.js
@@ -2,14 +2,15 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const errorhandler = require("errorhandler");
 const express = require("express");
+const apiRouter = require("./api/api");
 const app = express();
 const PORT = process.env.PORT || 4000; 
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(errorhandler());
 app.use(morgan("dev"));
-
+app.use("/api", apiRouter);
+app.use(errorhandler());
 
 app.listen(PORT, () => {
   console.log(`Listening on Port: ${PORT}`);
