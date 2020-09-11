890c23c97583862b0dbab76436d6f9546b4685d2
shashidar mandalozu <sasitech18@gmail.com>
2020-09-11 14:29:06 +0100

    Java project: Based On Inheritance And Polymorphism

---
+++ 104.LanguageFamilies/SinoTibetan.java
@@ -0,0 +1,8 @@
+class SinoTibetan extends Language {
+    SinoTibetan(String languageName, int speakers) {
+        super(languageName, speakers, "Asia", "subject-object-verb");
+        if (languageName.contains("Chinese")) {
+            this.wordOrder = "subject-verb-object";
+        }
+    }
+}
