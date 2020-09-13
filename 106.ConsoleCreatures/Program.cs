5ce8e3b94b5b232d98b309171549699183378c89
shashidar mandalozu <sasitech18@gmail.com>
2020-09-13 23:47:55 +0100

    C# Project: Based On Console knowledge

---
+++ 106.ConsoleCreatures/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      //Creating Console Creatures
+      Console.WriteLine(" .-.");   
+      Console.WriteLine("(o o)"); 
+      Console.WriteLine("| 0 |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("Spiro the Specter");
+     }
+  }
+}
