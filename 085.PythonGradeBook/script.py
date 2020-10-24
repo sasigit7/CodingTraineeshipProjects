daecdfe7018059814e55e16bfed225f1bcf3447f
shashidar mandalozu <sasitech18@gmail.com>
2020-10-24 17:59:26 +0100

    Python Project: Based On Lists

---
+++ 085.PythonGradeBook/script.py
@@ -0,0 +1,17 @@
+last_semester_gradebook = [
+    ("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
+
+subjects = ["physics", "calculus", "poetry", "history"]
+grades = [98, 97, 85, 88]
+
+subjects.append("computer science")
+grades.append(100)
+
+gradebook = list(zip(subjects, grades))
+
+gradebook.append(("visual arts", 93))
+
+print(gradebook)
+
+full_gradebook = last_semester_gradebook + gradebook
+print(full_gradebook)
